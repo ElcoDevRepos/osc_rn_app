45437582c15e6bda9f0912d1edf50bb909ae5c12
Austin Hunter <ahunter8850@outlook.com>
2023-04-04 13:15:36 -0500

    Add buttons to state

--- src/pages/remote.js
+++ src/pages/remote.js
@@ -18,18 +18,29 @@ import buttons from '../helpers/buttons';
 export default class Remote extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            buttons: buttons
+          };
         // All Messages from EOS Will come here. 
         // They still need to be decoded. 
         const eventEmitter = new NativeEventEmitter(tcpOsc);
         eventEmitter.addListener('GotMessage', (oscMessage) => {
             console.log("message: ", oscMessage);
+            this.alterButtonData();
         });
     }
 
+    alterButtonData() {
+        // You can alter your buttons data here.
+        // Then call the setState to update the buttons value
+        
+        //buttons[0].title = "Test";
+        this.setState(buttons);
+    }
 
     renderButtons() {
         const buttonObjects = [];
-        buttons.forEach((button) => {
+        this.state.buttons.forEach((button) => {
             buttonObjects.push(
                 (
                     <CustomButton title={button.title} id={button.attribute} key={button.attribute} onPress={() => {
@@ -46,9 +57,7 @@ export default class Remote extends React.Component {
             <View style={{
                 flexDirection: 'row'
             }}>
-
                 {this.renderButtons()}
-
             </View>
         );
     }
